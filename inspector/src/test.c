f99f0520059c30cbc919e9e043f750ef02d63fac
Douglas Amorim Ferreira <douglas@MacBook-Pro-de-Douglas.local>
2021-11-10 22:52:55 -0300

    adding one more test

--- inspector/src/test.c
+++ inspector/src/test.c
@@ -112,6 +112,35 @@ frame_header_test_004 (void)
   printf("\n");
 }
 
+void
+frame_header_test_005 (void) 
+{
+  // is a keyframe
+  FrameInfo frame;
+  unsigned char data[] = { 
+    0b10010001, // keyframe(1) = false, version(3) = 0, display(1) = true
+    0b00011000, 
+    0b00000110, // partSize(19) = 12484
+    0b00000000, // segmentationEnabled(1) = false, filter_type(1), loop_filter_level(6)
+    0b00000000, // sharpness_level(3), loop_filter_adj_enable(1) = false, log2_nbr_of_dct_partitions(2), y_ac_qi(2/7),
+    0b00000000, // y_ac_qi (5/7), y_dc_delta_present(1) = false, y2_dc_delta_present(1) = false, y2_ac_delta_present(1) = false
+    0b00110000, // y2_ac_delta_present(1) = false, uv_ac_delta_present(1) = false, refresh_golden_frame(1) = 1, refresh_alternate_frame(1) = 1
+  };
+
+  int version = 0;
+  int partSize = 12484;
+  unsigned int len = partSize + KEYFRAME_HEADER_SZ + FRAME_HEADER_SZ + 1;
+
+  printf("- Refresh Golden and Altref frames \n");
+  test_bool("Should parse all headers", vp8_parse_header(data, len, &frame) == VP8_CODEC_OK);
+  test_bool("Should not be a keyframe", !frame.keyframe);
+  test_bool("Should be display", frame.showFrame);
+  test_bool("Should get the correct version", frame.version == version);
+  test_bool("Should refresh the golden frame", frame.refreshGoldenFrame);
+  test_bool("Should refresh the altref frame", frame.refreshAltrefFrame);
+  printf("\n");
+}
+
 int
 main (int argc, char *argv[]) 
 {
@@ -119,5 +148,6 @@ main (int argc, char *argv[])
   frame_header_test_002();
   frame_header_test_003();
   frame_header_test_004();
+  frame_header_test_005();
   return 0;
 }
